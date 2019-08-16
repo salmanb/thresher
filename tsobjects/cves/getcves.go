405b24cbfccebc23612d54d858b7d06211aa2558
salmanb <namlas@gmail.com>
2019-08-16 11:47:30 -0400

    Update getcves.go

--- tsobjects/cves/getcves.go
+++ tsobjects/cves/getcves.go
@@ -9,6 +9,7 @@ import (
 
 // GetCVEByAgentID pulls list of CVEs from the TS API for specific agents
 // AgentID is expected as part of the apiendpoint URL string
+// TODO: Loop through CVEs by using the returned token, if it exists
 func GetCVEByAgentID(config tsapi.Config, apiendpoint string) ([]byte, error) {
 	req, err := apiclient.NewHttpReq(config, apiendpoint)
 	if err != nil {
